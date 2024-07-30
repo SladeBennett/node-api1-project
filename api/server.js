824e50a793e4306408dbac9835392c79a33aa0f9
SladeBennett <slade8colt@gmail.com>
2024-07-29 19:47:07 -0600

    new user post functional

--- api/server.js
+++ api/server.js
@@ -1,6 +1,29 @@
 const express = require('express')
 const User = require('./users/model')
+
 const server = express()
+server.use(express.json())
+
+server.post('/api/users', (req, res) => {
+    const user = req.body
+    if (!user.name || !user.bio) {
+        res.status(400).json({
+            message: 'Please provide name and bio for the user'
+        })
+    } else {
+        User.insert(user)
+            .then(createdUser => {
+                res.status(201).json(createdUser)
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: 'error creating user',
+                    err: err.message,
+                    stack: err.stack,
+                })
+            })
+    }
+})
 
 server.get('/api/users', async (req, res) => {
     User.find()
@@ -19,6 +42,11 @@ server.get('/api/users', async (req, res) => {
 server.get('/api/users/:id', async (req, res) => {
     User.findById(req.params.id)
         .then(user => {
+            if (!user) {
+                res.status(404).json({
+                    message: "The user with the specified ID does not exist",
+                })
+            }
             res.json(user)
         })
         .catch(err => {
